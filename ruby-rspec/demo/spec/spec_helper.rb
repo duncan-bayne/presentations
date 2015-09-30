79c921c122a27afff4da933a0e22110b4aa4fcc1
Duncan Bayne <duncan@bayne.id.au>
2015-09-30 14:58:11 +1000

    Wildcard glob in spec helper to automagically reference new files

--- ruby-rspec/demo/spec/spec_helper.rb
+++ ruby-rspec/demo/spec/spec_helper.rb
@@ -1,3 +1,7 @@
+['src'].each do |path|
+  Dir.glob(File.expand_path(File.join(path, '**', '*.rb'))).each { |file| require file }
+end
+
 RSpec.configure do |config|
   config.raise_errors_for_deprecations!
 end

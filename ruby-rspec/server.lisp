b83a8dacdce831af792fdd6c0c73347d6cb94368
Duncan Bayne <duncan@bayne.id.au>
2015-09-30 14:43:52 +1000

    Remote control the presentation with a phone, to antagonise Gareth

---
+++ ruby-rspec/server.lisp
@@ -0,0 +1,48 @@
+(require 'elnode)
+
+(setq org-present-port 8009)
+(setq org-present-host "10.10.10.230")
+
+(setq
+ org-present-html
+      "<!doctype html>
+       <style type=\"text/css\">
+         a {
+           font-size: 8em;
+           margin-right: 1em;
+         }
+       </style>
+       <html>
+         <body>
+           <a href=\"/prev\">Prev</a>
+           <a href=\"/next\">Next</a>
+         </body>
+       </html>")
+
+(setq elnode-error-log-to-messages nil)
+
+(defun org-present-prev-handler (httpcon)
+  (with-current-buffer (get-buffer "ruby-rspec.org") (org-present-prev))
+  (elnode-http-start httpcon 200 '("Content-type" . "text/html"))
+  (elnode-http-return httpcon org-present-html))
+
+(defun org-present-next-handler (httpcon)
+  (with-current-buffer (get-buffer "ruby-rspec.org") (org-present-next))
+  (elnode-http-start httpcon 200 '("Content-type" . "text/html"))
+  (elnode-http-return httpcon org-present-html))
+
+(defun org-present-default-handler (httpcon)
+  (elnode-http-start httpcon 200 '("Content-type" . "text/html"))
+  (elnode-http-return httpcon org-present-html))
+
+(setq
+ org-present-routes
+  '(("^.*//prev$" . org-present-prev-handler)
+    ("^.*//next$" . org-present-next-handler)
+    ("^.*//$" . org-present-default-handler)))
+
+(defun org-present-root-handler (httpcon)
+  (elnode-hostpath-dispatcher httpcon org-present-routes))
+
+(elnode-stop 8009)
+(elnode-start 'org-present-root-handler :port 8009 :host "10.10.10.230")
